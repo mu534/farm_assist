4075bd7b514a1fc3099d3f35286cfc3327f724e6
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-09-09 13:15:38 +0000

    Add PlantDisease model

---
+++ farmer_asist/lib/ui/models/plant_disease_model.dart
@@ -0,0 +1,11 @@
+class PlantDisease {
+  final String name;
+  final String description;
+  final String advice;
+
+  PlantDisease({
+    required this.name,
+    required this.description,
+    required this.advice,
+  });
+}
