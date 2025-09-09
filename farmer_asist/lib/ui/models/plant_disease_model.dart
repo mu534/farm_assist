dc3b47f4a1d2727e42111080d1e17f467d592137
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-09-09 13:39:08 +0000

    update ai_service and plant_disease model

--- farmer_asist/lib/ui/models/plant_disease_model.dart
+++ farmer_asist/lib/ui/models/plant_disease_model.dart
@@ -1,11 +1,11 @@
-class PlantDisease {
-  final String name;
-  final String description;
-  final String advice;
+class PlantDiseaseModel {
+  final String diseaseName;
+  final String recommendation;
+  final double confidence;
 
-  PlantDisease({
-    required this.name,
-    required this.description,
-    required this.advice,
+  PlantDiseaseModel({
+    required this.diseaseName,
+    required this.recommendation,
+    required this.confidence,
   });
 }
