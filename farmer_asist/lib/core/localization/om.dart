bee47df9780d3d3928453a6b9f6fd138a3208f62
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-09-10 08:05:05 +0000

    feat: add basic localization maps (en, am, om) and prepare for LocalizationService integration

---
+++ farmer_asist/lib/core/localization/om.dart
@@ -0,0 +1,22 @@
+const Map<String, String> om = {
+  'app_title': 'Gabbaraa Gargaarsa',
+  'splash_message': 'Dhukkuba biqilaa salphatti adda baafadhu',
+  'onboarding_title': 'Baga Nagaan Dhuftan Gara Gabbaraa Gargaarsa',
+  'onboarding_subtitle':
+      'Suuraa biqilaa kaappachuu ykn filachuudhaan dhukkuba adda baafadhu',
+  'home_camera': 'Kaameraa',
+  'home_gallery': 'Galmeewwan',
+  'home_settings': 'Qindaa\'inoota',
+  'capture_plant_image': 'Suuraa Biqilaa Kaappadhu',
+  'choose_photo': 'Suuraa Filadhu',
+  'plant_analysis': 'Xiinxala Biqilaa',
+  'detected_disease': 'Dhukkuba Argame:',
+  'recommended_treatment': 'Yaalii Fayyadamaa:',
+  'confidence': 'Amansiisaa:',
+  'back': 'Deebi\'i',
+  'no_images_found': 'Suuraaleen hin argamne',
+  'failed_to_take_picture': 'Suuraa kaasuuf hin danda\'ame.',
+  'failed_to_analyze_image':
+      'Suuraa xiinxaluun hin danda\'ame. Gara biraa yaali.',
+  'no_result_found': 'Bu\'aan hin argamne',
+};
