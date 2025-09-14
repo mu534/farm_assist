b2fa9b29a2004fb1870d9b1b858123ad7461db21
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-09-14 19:14:50 +0000

    update language_selector

--- farmer_asist/lib/ui/widgets/language_selector.dart
+++ farmer_asist/lib/ui/widgets/language_selector.dart
@@ -1,40 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'package:farmer_asist/ui/providers/language_provider.dart';
-import 'package:farmer_asist/ui/services/localization_service.dart';
 
 class LanguageSelector extends StatelessWidget {
-  const LanguageSelector({super.key});
+  final VoidCallback onEnglish;
+  final VoidCallback onAmharic;
+  final VoidCallback onOromo;
+
+  const LanguageSelector({
+    super.key,
+    required this.onEnglish,
+    required this.onAmharic,
+    required this.onOromo,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final languageProvider = Provider.of<LanguageProvider>(context);
-
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        TextButton(
-          onPressed: () async {
-            languageProvider.changeLanguage('en');
-            await LocalizationService().load(const Locale('en'));
-          },
-          child: const Text('EN'),
-        ),
-        TextButton(
-          onPressed: () async {
-            languageProvider.changeLanguage('am');
-            await LocalizationService().load(const Locale('am'));
-          },
-          child: const Text('AM'),
-        ),
-        TextButton(
-          onPressed: () async {
-            languageProvider.changeLanguage('om');
-            await LocalizationService().load(const Locale('om'));
-          },
-          child: const Text('OR'),
-        ),
+        TextButton(onPressed: onEnglish, child: const Text('EN')),
+        TextButton(onPressed: onAmharic, child: const Text('AM')),
+        TextButton(onPressed: onOromo, child: const Text('OR')),
       ],
     );
   }
